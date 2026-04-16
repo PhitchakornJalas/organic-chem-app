271b3dbba296b6c8f2db1912e3b478ae93b0e26e
phitchakorn <phitchakorn16351pp@gmail.com>
2026-04-16 13:09:39 +0700

    add search method
    suggestion search
    give output with detail page
    alert when not found

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
 from neo4j import GraphDatabase
 
 app = Flask(__name__)
+
+app.secret_key = 'pp222324'
 
 # ดึงค่าจาก environment ที่ตั้งไว้ใน docker-compose
 uri = os.getenv("NEO4J_URI", "bolt://neo4j:7687") 
@@ -26,8 +28,20 @@ def index():
         """
         results = session.run(query)
         functional_groups = [record.data() for record in results]
+
+    with driver.session() as session:
+        query = """
+        MATCH (f:FunctionalGroup)
+        OPTIONAL MATCH (i:Items)-[:FUNCTIONALGROUP_IS]->(f)
+        RETURN collect(distinct f.name_en) + collect(distinct f.name_th) + 
+            collect(distinct f.groupName) + collect(distinct i.name_th) AS all_names
+        """
+
+        names = session.run(query).single()['all_names']
+        # กรองค่าที่เป็น None ออก
+        search_suggestions = [n for n in names if n is not None]
         
-    return render_template('index.html', functional_groups=functional_groups)
+    return render_template('index.html', functional_groups=functional_groups, all_searchable_names=search_suggestions)
 
 @app.route('/details/<group_name>')
 def details(group_name):
@@ -46,6 +60,40 @@ def details(group_name):
         examples = result['examples']
         
     return render_template('details.html', group=group_data, examples=examples)
+
+@app.route('/search', methods=['POST'])
+def search():
+    search_text = request.form.get('organicChemSearch', '').strip()
+    
+    if not search_text:
+        return redirect(url_for('index'))
+    
+    with driver.session() as session:
+        query = """
+        MATCH (f:FunctionalGroup)
+        WHERE toLower(trim(f.name_en)) = toLower(trim($q))
+        OR toLower(trim(f.name_th)) = toLower(trim($q))
+        OR toLower(trim(f.groupName)) = toLower(trim($q))
+        RETURN f.name_en AS name_en
+
+        UNION
+
+        MATCH (i:Items)-[:FUNCTIONALGROUP_IS]->(f:FunctionalGroup)
+        WHERE toLower(trim(i.name_th)) = toLower(trim($q))
+        OR toLower(trim(i.name_en)) = toLower(trim($q))
+        RETURN f.name_en AS name_en
+
+        LIMIT 1
+        """
+
+        result = session.run(query, q=search_text).single()
+        
+        if result:
+            return redirect(url_for('details', group_name=result['name_en']))
+        else:
+            # ถ้าพิมพ์เองแล้วไม่ตรงกับที่มีในระบบเลย
+            flash("ไม่พบข้อมูลสารหรือสิ่งของที่คุณค้นหา", "error")
+            return redirect(url_for('index'))
 
 if __name__ == '__main__':
     # host='0.0.0.0' สำคัญมากเพื่อให้เข้าถึงจากนอก Container ได้
