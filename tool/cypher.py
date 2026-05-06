ea6b8a4c067369cb784f892a150645b270733d16
phitchakorn <phitchakorn16351pp@gmail.com>
2026-05-06 09:39:37 +0700

    Update cypher.py
    upgrade format relation type_of

--- tool/cypher.py
+++ tool/cypher.py
@@ -18,13 +18,17 @@ def chemical(file_name):
         IUPAC = row.iloc[1]
         molecularFormula = row.iloc[2]
         
-        print(f"""(c{c_id}:Chemical {{IUPAC: "{IUPAC}", molecularFormula: "{molecularFormula}", createdAt: datetime({{timezone: '+07:00'}})}}),""")
+        print(f"""(c{c_id}:Chemical {{IUPAC: "{IUPAC}", molecularFormula: "{molecularFormula}", createdAt: datetime({{timezone: '+07:00'}})}}),<br>""")
 
-    for index, row in df.iterrows():
-        c_id = row.iloc[0]
-        f_id = row.iloc[3]
+    grouped = df.groupby(df.columns[3])
+
+    for f_name, group in grouped:
+        iupac_list = group.iloc[:, 1].tolist()
+        iupac_formatted = ", ".join([f'"{name}"' for name in iupac_list])
         
-        print(f'(c{c_id})-[:TYPE_OF]->(f{f_id}),')
+        print(f'MATCH (f:FunctionalGroup {{name_en: "{f_name}"}})<br>')
+        print(f'MATCH (c:Chemical) WHERE c.IUPAC IN [{iupac_formatted}]<br>')
+        print(f'CREATE (c)-[:TYPE_OF]->(f);<br>')
 
 if __name__ == "__main__":
     chemical('chem.xlsx')
